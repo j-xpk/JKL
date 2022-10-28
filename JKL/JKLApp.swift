5a2422829f392d3cbfb85eda2e862d1781be628f
Josie Kelly <jae.k.service@gmail.com>
2022-10-28 05:48:03 -0400

    Initial Commit

---
+++ JKL/JKLApp.swift
@@ -0,0 +1,17 @@
+//
+//  JKLApp.swift
+//  JKL
+//
+//  Created by jkelly on 10/28/22.
+//
+
+import SwiftUI
+
+@main
+struct JKLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
